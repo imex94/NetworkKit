c3ccbfe99e1a74e636cd5298e7abfad2b9ce0516
Alex Telek <imex94@gmail.com>
2016-03-17 18:41:49 +0000

    updated travis

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-xcodebuild -workspace NetworkKit.xcodeproj -scheme "NetworkKit" -destination "platform=iOS Simulator,name=iPhone 6" test
-xcodebuild -workspace NetworkKit.xcodeproj -scheme "NetworkKitMac" test
-xcodebuild -workspace NetworkKit.xcodeproj -scheme "NetworkKitTvOS" -destination "platform=tvOS Simulator,name=Apple TV 1080p" test
+xcodebuild -project NetworkKit.xcodeproj -scheme "NetworkKit" -destination "platform=iOS Simulator,name=iPhone 6" test
