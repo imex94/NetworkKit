0fbf7b66a036dddf8d67a4daf431d32b1e60ec80
Alex Telek <imex94@gmail.com>
2016-03-18 19:09:26 +0000

    modified travis

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,5 +2,4 @@
 
 set -e
 
-xcodebuild -project NetworkKit.xcodeproj -scheme NetworkKit -sdk iphonesimulator8.0 -configuration Debug clean build
 xcodebuild -project NetworkKit.xcodeproj -scheme NetworkKit -destination "platform=iOS Simulator,name=iPhone 6" test
